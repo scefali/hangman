c49f521ce52443d8364c25cd6145717ecbb3b97d
Stephen Cefali <stephencefali@gmail.com>
2020-01-08 21:44:00 -0800

    show letters used

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -32,7 +32,8 @@ defmodule Hangman.Game do
     %{
       game_state: game.game_state,
       turns_left: game.turns_left,
-      letters: game.letters |> reveal_guessed(game.used)
+      letters: game.letters |> reveal_guessed(game.used),
+      used: MapSet.to_list(game.used)
     }
   end
 
